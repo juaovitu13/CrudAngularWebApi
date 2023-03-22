9d5831f0442ea80a9a66e6cd44e4d24fb9ce973b
juaovitu13 <joao.victorsoares@outlook.com>
2023-03-22 14:23:24 -0300

    Alguns Metodos da controller criados

--- CrudApi/Controllers/CarroController.cs
+++ CrudApi/Controllers/CarroController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CrudApi.Context;
+using CrudApi.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CrudApi.Controllers
@@ -10,6 +12,36 @@ namespace CrudApi.Controllers
     [Route("[controller]")]
     public class CarroController : ControllerBase
     {
-        
+        private readonly CarroContext _context;
+        public CarroController(CarroContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var carro = _context.Carros.Find(id);
+
+            if(carro == null)
+               return NotFound();
+            return Ok(carro);
+        }
+
+        [HttpGet("ObterTodos")]
+        public IActionResult ObterTodos()
+        {
+            var carro = _context.Carros.ToList();
+            return Ok(carro);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Carro carro)
+        {
+            _context.Add(carro);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(ObterPorId), new {id = carro.Id}, carro);
+        }
+
     }
 }
