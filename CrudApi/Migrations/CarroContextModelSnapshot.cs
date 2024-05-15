8e0f5a9c205ca515a28e3da1afeed2f754f18554
juaovitu13 <joao.victorsoares@outlook.com>
2024-05-15 09:46:26 -0300

    Atualizando Versão do DotNet

--- CrudApi/Migrations/CarroContextModelSnapshot.cs
+++ CrudApi/Migrations/CarroContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace CrudApi.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.9")
+                .HasAnnotation("ProductVersion", "8.0.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
